ff61facd9ec832212663a9a31fac9005bbe4bffe
venkat_211 <venkat92001@gmail.com>
2026-06-25 09:42:42 +0530

    server check

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins CI/CD!');
+  res.send('Hello from Jenkins CI/CD!\ hi there');
 });
 
 app.listen(3000, () => {
